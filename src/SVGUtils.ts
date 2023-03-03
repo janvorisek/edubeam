2f26ed6332699c8988e088660272192bc760b12c
Jan Vorisek <jan@vorisek.me>
2023-03-03 21:52:45 +0100

    feat: show nodal/element loads and reactions

--- src/SVGUtils.ts
+++ src/SVGUtils.ts
@@ -182,7 +182,8 @@ export function formatElementFibers(el: Beam2D, scale) {
 }
 
 export function formatElementAngle(el: Beam2D) {
-  return Math.atan2(el.computeGeo().dz, el.computeGeo().dx) * (180 / Math.PI);
+  const geo = el.computeGeo();
+  return Math.atan2(geo.dz, geo.dx) * (180 / Math.PI);
 }
 
 export function formatElementLabel(el: Beam2D, scale: number, amount = 10) {
@@ -206,6 +207,16 @@ export function formatElementHinge(target: Beam2D, i: number, scale: number) {
   return line1;
 }
 
+export function formatNodalLoad(nl: NodalLoad, scale: number) {
+  const size = Math.sqrt(nl.values[0]! * nl.values[0]! + nl.values[2]! * nl.values[2]!);
+  const sx = -nl.values[0]! / size;
+  const sz = -nl.values[2]! / size;
+
+  return `${nl.domain.nodes.get(nl.target)!.coords[0]},${nl.domain.nodes.get(nl.target)!.coords[2]} ${
+    nl.domain.nodes.get(nl.target)!.coords[0] + (sx * 40) / scale
+  },${nl.domain.nodes.get(nl.target)!.coords[2] + (sz * 40) / scale}`;
+}
+
 export function formatNodalLoadAngle(nl: NodalLoad) {
   /*const dofs = this.domain.solver.getNodeDofIDs(nl.target);
   const ans = Array<number>();
@@ -223,21 +234,163 @@ export function formatNodalLoadAngle(nl: NodalLoad) {
 }
 
 export function formatElementLoad(eload: BeamElementUniformEdgeLoad, scale: number) {
+  if (eload.values[0] === 0 && eload.values[1] === 0) return "";
+
   const target = eload.domain.elements.get(eload.target) as Beam2D;
 
   const n1 = eload.domain.nodes.get(target.nodes[0]) as Node;
   const n2 = eload.domain.nodes.get(target.nodes[1]) as Node;
 
   const geo = target.computeGeo();
-  const nx = (geo.dz * 40) / geo.l / scale;
-  const nz = (-geo.dx * 40) / geo.l / scale;
 
-  const line1 = `${n1.coords[0]},${n1.coords[2]} ${n2.coords[0]},${n2.coords[2]}`;
-  const line2 = `${n2.coords[0] + nx},${n2.coords[2] + nz} ${n1.coords[0] + nx},${n1.coords[2] + nz} ${n1.coords[0]},${
-    n1.coords[2]
-  }`;
+  const smallestsize = eload.values[0] !== 0 && eload.values[1] === 0 ? 20 : 40;
 
-  return line1 + " " + line2;
+  const nx = (geo.dz * smallestsize) / geo.l / scale;
+  const nz = (-geo.dx * smallestsize) / geo.l / scale;
+  const nx2 = (geo.dz * 60) / geo.l / scale;
+  const nz2 = (-geo.dx * 60) / geo.l / scale;
+
+  let line1 = "",
+    line2 = "",
+    line3 = "";
+
+  if (eload.lcs) {
+    line1 = `${n1.coords[0]},${n1.coords[2]} ${n2.coords[0]},${n2.coords[2]}`;
+
+    line2 = `${n2.coords[0] + nx},${n2.coords[2] + nz} ${n1.coords[0] + nx},${n1.coords[2] + nz} ${n1.coords[0]},${
+      n1.coords[2]
+    }`;
+
+    line3 =
+      eload.values[0] !== 0 && eload.values[1] !== 0
+        ? `${n2.coords[0]},${n2.coords[2]} ${n2.coords[0] + nx2},${n2.coords[2] + nz2} ${n1.coords[0] + nx2},${
+            n1.coords[2] + nz2
+          }`
+        : ``;
+  } else {
+    if (eload.values[1] !== 0) {
+      line1 = `${n1.coords[0]},${n1.coords[2]} ${n1.coords[0]},${n1.coords[2] - 40 / scale} ${n2.coords[0]},${
+        n2.coords[2] - 40 / scale
+      } ${n2.coords[0]},${n2.coords[2]}`;
+    }
+
+    if (eload.values[0] !== 0) {
+      line2 = `${n1.coords[0]},${n1.coords[2]} ${n1.coords[0] - (Math.sign(nx) * 40) / scale},${n1.coords[2]} ${
+        n2.coords[0] - (Math.sign(nx) * 40) / scale
+      },${n2.coords[2]} ${n2.coords[0]},${n2.coords[2]}`;
+    }
+  }
+
+  return line1 + " " + line2 + " " + line3;
+}
+
+export function formatElementLoadForces(eload: BeamElementUniformEdgeLoad, scale: number, component: number) {
+  const target = eload.domain.elements.get(eload.target) as Beam2D;
+
+  const n1 = eload.domain.nodes.get(target.nodes[0]) as Node;
+
+  const geo = target.computeGeo();
+  const nx = geo.dz / geo.l / scale;
+  const nz = -geo.dx / geo.l / scale;
+  const nnx = (geo.dz * 40) / geo.l / scale;
+  const nnz = (-geo.dx * 40) / geo.l / scale;
+
+  let nseg = Math.ceil((geo.l * scale) / 20);
+  let segsize = geo.l / nseg;
+  const cos = geo.dx / geo.l;
+  const sin = geo.dz / geo.l;
+
+  const pts: [number, number][] = [];
+
+  if (eload.lcs) {
+    if (component === 1) {
+      const moveDist = eload.values[1] >= 0 ? 0 : 2;
+
+      for (let s = 0; s <= nseg; s++) {
+        pts.push([
+          n1.coords[0] + s * cos * segsize + nx * 20 + nx * moveDist,
+          n1.coords[2] + s * sin * segsize + nz * 20 + nz * moveDist,
+        ]);
+      }
+    }
+    if (component === 0) {
+      nseg = Math.floor((geo.l * scale) / 45);
+      segsize = geo.l / nseg;
+      const mx = (cos * segsize) / 2;
+      const mz = (sin * segsize) / 2;
+      const moveDist = eload.values[0] !== 0 && eload.values[1] === 0 ? 10 : 50;
+      for (let s = 0; s < nseg; s++) {
+        pts.push([
+          n1.coords[0] + mx + s * cos * segsize + nx * moveDist,
+          mz + n1.coords[2] + s * sin * segsize + nz * moveDist,
+        ]);
+      }
+    }
+  } else {
+    if (component === 1) {
+      for (let s = 0; s <= nseg; s++) {
+        pts.push([n1.coords[0] + s * cos * segsize, n1.coords[2] + s * sin * segsize - 20 / scale]);
+      }
+    }
+    if (component === 0) {
+      for (let s = 0; s <= nseg; s++) {
+        pts.push([n1.coords[0] + s * cos * segsize - (Math.sign(nx) * 20) / scale, n1.coords[2] + s * sin * segsize]);
+      }
+    }
+  }
+
+  return pts;
+}
+
+export function formatElementLoadLabel(eload: BeamElementUniformEdgeLoad, scale: number, component: number) {
+  const target = eload.domain.elements.get(eload.target) as Beam2D;
+  const geo = target.computeGeo();
+  const nz = geo.dx / geo.l;
+  const nx = -geo.dz / geo.l;
+
+  const n1 = eload.domain.nodes.get(target.nodes[0]) as Node;
+  const n2 = eload.domain.nodes.get(target.nodes[1]) as Node;
+
+  const ncx = n1.coords[0]; //(n1.coords[0] + n2.coords[0]) / 2;
+  const ncz = n1.coords[2]; //(n1.coords[2] + n2.coords[2]) / 2;
+
+  const angle = 0; //Math.atan2(geo.dz, geo.dx) * (180 / Math.PI);
+  const dist = 40;
+  const dist2 = eload.values[1] !== 0 ? 60 : 20;
+
+  const dx = eload.lcs ? (-nx * dist) / scale : 0;
+  const dz = eload.lcs ? (-nz * dist) / scale : -dist / scale;
+
+  const dx3 = -(nz * 5) / scale;
+  const dz3 = (nx * 5) / scale;
+
+  if (eload.lcs) {
+    const dx2 = (-nx * dist2) / scale;
+    const dz2 = (-nz * dist2) / scale;
+
+    if (component === 0) return `translate(${ncx + dx2 + dx3}, ${ncz + dz2 + dz3}) rotate(${angle})`;
+    if (component === 1) return `translate(${ncx + dx + dx3}, ${ncz + dz + dz3}) rotate(${angle})`;
+  } else {
+    if (component === 0) return `translate(${ncx - dist / scale + dx3}, ${ncz + dz3}) rotate(${angle})`;
+    if (component === 1) return `translate(${ncx + dx3}, ${ncz - dist / scale + dz3}) rotate(${angle})`;
+  }
+
+  return `translate(${ncx + dx}, ${ncz + dz}) rotate(${angle})`;
+}
+
+export function formatElementLoadForcesAngle(eload: BeamElementUniformEdgeLoad, component: number) {
+  if (!eload.lcs) {
+    if (component === 0) return eload.values[0] < 0 ? 90 : 270;
+    else if (component === 1) return eload.values[1] > 0 ? 0 : 180;
+  }
+
+  const target = eload.domain.elements.get(eload.target) as Beam2D;
+  const geo = target.computeGeo();
+
+  if (component === 0) return (Math.atan2(geo.dz, geo.dx) * 180) / Math.PI - 90 * Math.sign(eload.values[0]);
+
+  if (eload.values[1] < 0) return (Math.atan2(geo.dz, geo.dx) * 180) / Math.PI + 180;
+  return (Math.atan2(geo.dz, geo.dx) * 180) / Math.PI;
 }
 
 export function formatElementLoadHatch(eload: BeamElementUniformEdgeLoad, scale: number) {
@@ -256,16 +409,56 @@ export function formatElementLoadHatch(eload: BeamElementUniformEdgeLoad, scale:
   const sizeScale = 1;
   const cos = geo.dx / geo.l;
   const sin = geo.dz / geo.l;
-  for (let s = 0; s <= nseg; s++) {
-    const xc = n1.coords[0] + (cos * geo.l * s) / nseg;
-    const zc = n1.coords[2] + (sin * geo.l * s) / nseg;
 
-    result += `M${xc + nx * 10 - (geo.dx * 5) / geo.l / scale},${
-      zc - (geo.dz / geo.l / scale) * 5 + nz * 10
-    } L${xc},${zc} L${xc + nx * 10 + ((geo.dx / geo.l) * 5) / scale},${
-      zc + (geo.dz / geo.l / scale) * 5 + nz * 10
-    } M${xc},${zc} L${xc + nnx * sizeScale},${zc + nnz * sizeScale}`;
+  if (eload.values[1] !== 0) {
+    for (let s = 0; s <= nseg; s++) {
+      const xc = n1.coords[0] + (cos * geo.l * s) / nseg;
+      const zc = n1.coords[2] + (sin * geo.l * s) / nseg;
+
+      if (eload.values[1] > 0) {
+        result += `M${xc + nx * 10 - (geo.dx * 5) / geo.l / scale},${
+          zc - (geo.dz / geo.l / scale) * 5 + nz * 10
+        } L${xc},${zc} L${xc + nx * 10 + ((geo.dx / geo.l) * 5) / scale},${
+          zc + (geo.dz / geo.l / scale) * 5 + nz * 10
+        } `;
+      } else {
+        result += `M${xc + nnx * sizeScale - nx * 10 - ((geo.dx / geo.l) * 5) / scale},${
+          zc + nnz * sizeScale - nz * 10 - ((geo.dz / geo.l) * 5) / scale
+        }`;
+
+        result += `L${xc + nnx * sizeScale},${zc + nnz * sizeScale}`;
+
+        result += `L${xc + nnx * sizeScale - nx * 10 + ((geo.dx / geo.l) * 5) / scale},${
+          zc + nnz * sizeScale - nz * 10 + ((geo.dz / geo.l) * 5) / scale
+        }`;
+      }
+
+      result += `M${xc},${zc} L${xc + nnx * sizeScale},${zc + nnz * sizeScale}`;
+    }
   }
 
+  if (eload.values[0] !== 0) {
+    let dfromfz = 0;
+    if (eload.values[1] !== 0) dfromfz = 40;
+    const nseg2 = Math.ceil((geo.l * scale) / 40);
+    for (let s = 0; s < nseg2; s++) {
+      const xc = n1.coords[0] + (cos * geo.l * s) / nseg2 + nx * dfromfz;
+      const zc = n1.coords[2] + (sin * geo.l * s) / nseg2 + nz * dfromfz;
+
+      if (eload.values[0] < 0) {
+        result += `M${xc + nx * 20}, ${zc + nz * 20} L${xc + nx * 25 - nz * 10},${zc + nz * 25 + nx * 10}`;
+        result += `M${xc + nx * 20}, ${zc + nz * 20} L${xc + nx * 15 - nz * 10},${zc + nz * 15 + nx * 10}`;
+      } else {
+        result += `M${xc + nx * 20 - nz * 30},${zc + nz * 20 + nx * 30} L${xc + nx * 25 - nz * 20},${
+          zc + nz * 25 + nx * 20
+        }`;
+        result += `M${xc + nx * 20 - nz * 30},${zc + nz * 20 + nx * 30} L${xc + nx * 15 - nz * 20},${
+          zc + nz * 15 + nx * 20
+        }`;
+      }
+
+      result += `M${xc + nx * 20}, ${zc + nz * 20} L${xc + nx * 20 - nz * 30},${zc + nz * 20 + nx * 30}`;
+    }
+  }
   return result;
 }

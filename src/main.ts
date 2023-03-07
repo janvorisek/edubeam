444572bfbfffe396ee969344a7591c71ffa8a6bf
Jan Vorisek <jan@vorisek.me>
2023-03-07 17:27:00 +0100

    feat: add google analytics to production build

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ import App from "./App.vue";
 // Composables
 import { createApp } from "vue";
 
+// Google Analytics
+import VueGtag from "vue-gtag";
+
 // Plugins
 import { registerPlugins } from "@/plugins";
 
@@ -18,6 +21,12 @@ import "./assets/main.scss";
 const app = createApp(App);
 
 app.config.globalProperties.window = window;
+
+if (import.meta.env.PROD) {
+  app.use(VueGtag, {
+    config: { id: "G-RFM73PGN79" },
+  });
+}
 
 registerPlugins(app);
 

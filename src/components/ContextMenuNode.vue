90e59d69bf4f5f2a9d7e638be8c1d8094f7b5e7c
Jan Vorisek <jan@vorisek.me>
2024-02-18 16:48:01 +0100

    feat: update node supports from click menu

--- src/components/ContextMenuNode.vue
+++ src/components/ContextMenuNode.vue
@@ -2,12 +2,57 @@
 import { openModal } from "jenesius-vue-modal";
 import AddNodalLoadDialog from "./dialogs/AddNodalLoad.vue";
 import { useProjectStore } from "@/store/project";
+import { toggleSet } from "@/utils";
+import { computed } from "vue";
 
 const projectStore = useProjectStore();
+
+const node = computed(() => {
+  return projectStore.solver.domain.nodes.get(projectStore.selection.label);
+});
 </script>
 
 <template>
   <v-list density="compact" class="py-0">
+    <v-list-item link class="text-body-2">
+      <template #prepend>
+        <div class="pr-2"><v-icon size="16" icon="mdi-triangle-outline" /></div>
+      </template>
+      {{ $t("nodes.defineSupports") }}
+      <v-menu activator="parent" open-on-hover location="end" :close-on-content-click="false">
+        <v-list density="compact" class="py-0">
+          <v-row no-gutters class="px-1 text-no-wrap" style="word-break: keep-all">
+            <v-col>
+              <v-checkbox
+                density="compact"
+                label="Dx"
+                hide-details="auto"
+                :model-value="node.bcs.has(0)"
+                @click="toggleSet(node, 'bcs', 0)"
+              ></v-checkbox>
+            </v-col>
+            <v-col>
+              <v-checkbox
+                density="compact"
+                label="Dz"
+                hide-details="auto"
+                :model-value="node.bcs.has(2)"
+                @click="toggleSet(node, 'bcs', 2)"
+              ></v-checkbox>
+            </v-col>
+            <v-col>
+              <v-checkbox
+                density="compact"
+                label="Ry"
+                hide-details="auto"
+                :model-value="node.bcs.has(4)"
+                @click="toggleSet(node, 'bcs', 4)"
+              ></v-checkbox>
+            </v-col>
+          </v-row>
+        </v-list>
+      </v-menu>
+    </v-list-item>
     <v-list-item
       link
       class="text-body-2"
@@ -31,7 +76,7 @@ const projectStore = useProjectStore();
       "
     >
       <template #prepend>
-        <div class="pr-2"><v-icon size="16" icon="mdi-arrow-down-thin" /></div>
+        <div class="pr-2"><v-icon size="16" icon="mdi-ruler" /></div>
       </template>
       {{ $t("loads.addPrescribedDisplacement") }}
     </v-list-item>

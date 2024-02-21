6b04bcc838fb0285e9cbe2f524e2615271f716d3
Jan Vorisek <jan@vorisek.me>
2024-02-21 15:21:55 +0100

    feat: show user feed back on error

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,13 @@ if (import.meta.env.PROD) {
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    beforeSend(event, hint) {
+      // Check if it is an exception, and if so, show the report dialog
+      if (event.exception && event.event_id) {
+        Sentry.showReportDialog({ eventId: event.event_id });
+      }
+      return event;
+    },
   });
 }
 

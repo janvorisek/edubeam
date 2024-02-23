c1e4254229114c453d408025eeac4e48d3171407
Jan Vorisek <jan@vorisek.me>
2024-02-23 16:40:00 +0100

    chore: remove unused css

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -47,28 +47,3 @@ async function update() {
     </v-card>
   </v-dialog>
 </template>
-
-<style>
-.pwa-toast {
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  margin: 16px;
-  padding: 12px;
-  border: 1px solid #8885;
-  border-radius: 4px;
-  z-index: 1;
-  text-align: left;
-  box-shadow: 3px 4px 5px 0px #8885;
-}
-.pwa-toast .message {
-  margin-bottom: 8px;
-}
-.pwa-toast button {
-  border: 1px solid #8885;
-  outline: none;
-  margin-right: 5px;
-  border-radius: 2px;
-  padding: 3px 10px;
-}
-</style>

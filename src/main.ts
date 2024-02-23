24d511731c07843be2d84f969f9e1478deaf5f65
Jan Vorisek <jan@vorisek.me>
2024-02-23 11:46:47 +0100

    chore: remove useless SW code

--- src/main.ts
+++ src/main.ts
@@ -54,31 +54,6 @@ if (import.meta.env.PROD) {
   });
 }
 
-const intervalMS = 60 * 60 * 1000;
-
-const updateSW = registerSW({
-  onRegisteredSW(swUrl, r) {
-    r &&
-      setInterval(async () => {
-        if (!(!r.installing && navigator)) return;
-
-        if ("connection" in navigator && !navigator.onLine) return;
-
-        const resp = await fetch(swUrl, {
-          cache: "no-store",
-          headers: {
-            cache: "no-store",
-            "cache-control": "no-cache",
-          },
-        });
-
-        if (resp?.status === 200) await r.update();
-      }, intervalMS);
-  },
-});
-
-updateSW();
-
 registerPlugins(app);
 
 app.mount("#app");

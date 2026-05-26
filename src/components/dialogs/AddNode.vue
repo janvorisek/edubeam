72b063221d9241f5984122d92c6d4fe799298b69
Jan Vorisek <jan@vorisek.me>
2026-05-26 09:41:16 +0200

    feat: Nodal supports and LCS can be defined in Add Node Dialog

--- src/components/dialogs/AddNode.vue
+++ src/components/dialogs/AddNode.vue
@@ -5,36 +5,91 @@
 
       <v-card-text>
         <v-form v-model="valid">
-          <v-container>
-            <v-row no-gutters>
-              <v-col cols="6" md="6">
-                <v-text-field
-                  v-model="newNodeX"
-                  :label="$t('dialogs.addNode.coordinate_x')"
-                  :suffix="appStore.units.Length"
-                  hide-details="auto"
-                  :rules="numberRules"
-                  autofocus
-                  required
-                  :rounded="0"
-                  @keydown="checkNumber($event)"
-                ></v-text-field>
-              </v-col>
+          <v-row>
+            <v-col cols="12">
+              <v-row no-gutters>
+                <v-col cols="6">
+                  <v-text-field
+                    v-model="newNodeX"
+                    :label="$t('dialogs.addNode.coordinate_x')"
+                    :suffix="appStore.units.Length"
+                    hide-details="auto"
+                    :rules="numberRules"
+                    autofocus
+                    required
+                    :rounded="0"
+                    @keydown="checkNumber($event)"
+                  ></v-text-field>
+                </v-col>
 
-              <v-col cols="6" md="6">
-                <v-text-field
-                  v-model="newNodeZ"
-                  :label="$t('dialogs.addNode.coordinate_z')"
-                  :suffix="appStore.units.Length"
-                  hide-details="auto"
-                  :rules="numberRules"
-                  required
-                  :rounded="0"
-                  @keydown="checkNumber($event)"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
+                <v-col cols="6">
+                  <v-text-field
+                    v-model="newNodeZ"
+                    :label="$t('dialogs.addNode.coordinate_z')"
+                    :suffix="appStore.units.Length"
+                    hide-details="auto"
+                    :rules="numberRules"
+                    required
+                    :rounded="0"
+                    @keydown="checkNumber($event)"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-col>
+
+            <v-col cols="12">
+              <div class="d-flex justify-center mb-6" style="height: 64px">
+                <SupportHelper :angle="angleVal" :node="tmpNode" />
+              </div>
+              <div>
+                <v-row no-gutters>
+                  <v-col cols="6">
+                    <div class="text-caption">{{ $t('dofs.bcs') }}</div>
+                    <v-row no-gutters>
+                      <v-col cols="4">
+                        <v-checkbox
+                          density="compact"
+                          label="Dx"
+                          hide-details="auto"
+                          :model-value="tmpNode.bcs.has(0)"
+                          @click="toggleSet(tmpNode, 'bcs', 0)"
+                        ></v-checkbox>
+                      </v-col>
+                      <v-col cols="4">
+                        <v-checkbox
+                          density="compact"
+                          label="Dz"
+                          hide-details="auto"
+                          :model-value="tmpNode.bcs.has(2)"
+                          @click="toggleSet(tmpNode, 'bcs', 2)"
+                        ></v-checkbox>
+                      </v-col>
+                      <v-col cols="4">
+                        <v-checkbox
+                          density="compact"
+                          label="Ry"
+                          hide-details="auto"
+                          :model-value="tmpNode.bcs.has(4)"
+                          @click="toggleSet(tmpNode, 'bcs', 4)"
+                        ></v-checkbox>
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                  <v-col cols="6">
+                    <v-text-field
+                      v-model="nodalAngle"
+                      :label="$t('nodes.lcsAngle')"
+                      hide-details="auto"
+                      :rules="[...numberRules, minMax]"
+                      :suffix="`°`"
+                      :rounded="0"
+                      @keydown="checkNumber($event)"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </div>
+            </v-col>
+          </v-row>
         </v-form>
       </v-card-text>
 
@@ -52,11 +107,13 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { useProjectStore } from '../../store/project';
+import { Node } from 'ts-fem';
 import { closeModal } from 'jenesius-vue-modal';
 import { useAppStore } from '@/store/app';
-import { checkNumber, changeRefNumValue, executeModelMutationWithUndo, numberRules } from '../../utils';
+import SupportHelper from '../svg/SupportHelper.vue';
+import { checkNumber, changeRefNumValue, executeModelMutationWithUndo, numberRules, toggleSet } from '../../utils';
 
 const projectStore = useProjectStore();
 const appStore = useAppStore();
@@ -66,11 +123,25 @@ const valid = ref(false);
 
 const newNodeX = ref('0');
 const newNodeZ = ref('0');
+const nodalAngle = ref('0');
+const tmpNode = ref(new Node('tmp', undefined, [0, 0, 0]));
+
+const angleVal = computed(() => {
+  const val = parseFloat(nodalAngle.value);
+  return isNaN(val) ? 0 : val;
+});
+
+const minMax = (v) => {
+  const val = parseFloat(v);
+  if (val < -180 || val > 180) {
+    return 'Enter value between -180 and 180.';
+  }
+  return true;
+};
 
 const addNode = () => {
   if (valid.value === false) return;
 
-  useProjectStore().solver.loadCases[0].solved = false;
   const domain = projectStore.solver.domain;
 
   let nid = domain.nodes.size + 1; //Math.round(Date.now() / 1000);
@@ -81,9 +152,17 @@ const addNode = () => {
 
   const nx = appStore.convertInverseLength(changeRefNumValue(newNodeX.value.toString()));
   const nz = appStore.convertInverseLength(changeRefNumValue(newNodeZ.value.toString()));
+  const ang = parseFloat(nodalAngle.value) * (Math.PI / 180);
 
   executeModelMutationWithUndo(() => {
-    domain.createNode(nid, [nx, 0.0, nz]);
+    const node = domain.createNode(nid, [nx, 0.0, nz], [...tmpNode.value.bcs.values()]);
+
+    if (!isNaN(ang) && Math.abs(ang) >= 1e-8) {
+      const locx = [Math.cos(ang), 0, Math.sin(ang)];
+      const locy = [0, 1, 0];
+      node.updateLcs({ locx, locy });
+    }
+
     domain.nodes = new Map(domain.nodes);
   });
 
